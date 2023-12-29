708775410fc0fae97c82791aa331302c2bd74286
Pavel <143156330+pavel844@users.noreply.github.com>
2023-12-29 07:41:28 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,14 @@
 
-def main():
-        print(f"Random Number: {item}")
-    return data
+
 
 import random
+    return data
+def main():
+    for item in data:
+    data = generate_random_data()
+    main()
+        print(f"Random Number: {item}")
 
 def generate_random_data():
-    data = generate_random_data()
 if __name__ == "__main__":
-    main()
     data = [random.randint(1, 100) for _ in range(10)]
-
-    for item in data:
